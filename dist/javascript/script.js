6a759edc1f0d8d9ce5334e00694fc94039b88b0e
AlexSaxena <saxenaalex@gmail.com>
2022-11-20 15:52:14 +0100

    Adds JQuery for slideToggle card info

--- dist/javascript/script.js
+++ dist/javascript/script.js
@@ -1,1 +1,32 @@
 console.log("Hello There!");
+$(document).ready(function () {
+  $(".btn-card-more-info").text("Show More");
+
+  $(".toggle-card-1").click(function () {
+    extendCard(this, 1);
+  });
+  $(".toggle-card-2").click(function () {
+    extendCard(this, 2);
+  });
+  $(".toggle-card-3").click(function () {
+    extendCard(this, 3);
+  });
+  $(".toggle-card-4").click(function () {
+    extendCard(this, 4);
+  });
+  $(".toggle-card-5").click(function () {
+    extendCard(this, 5);
+  });
+  $(".toggle-card-6").click(function () {
+    extendCard(this, 6);
+  });
+
+  function extendCard(btnTag, cardNum) {
+    $(".slide-more-info-" + cardNum).slideToggle();
+    if ($(btnTag).text() == "Show More") {
+      $(btnTag).text("Show Less");
+    } else {
+      $(btnTag).text("Show More");
+    }
+  }
+});
